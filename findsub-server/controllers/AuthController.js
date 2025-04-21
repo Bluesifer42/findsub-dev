8b7c0ee81fb42900c20b3d74848539956b0cad1a
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:58:41 +0100

    Create AuthController.js

---
+++ findsub-server/controllers/AuthController.js
@@ -0,0 +1,11 @@
+exports.loginUser = (req, res) => {
+  res.send('loginUser not implemented');
+};
+
+exports.registerUser = (req, res) => {
+  res.send('registerUser not implemented');
+};
+
+exports.getCurrentUser = (req, res) => {
+  res.send('getCurrentUser not implemented');
+};
