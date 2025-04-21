e599f29c459213a112973db228d45fe15aa09d14
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:42:53 +0100

    Update FeedbackRoutes.js

--- findsub-server/routes/FeedbackRoutes.js
+++ findsub-server/routes/FeedbackRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const feedbackController = require('../controllers/feedbackController');
 
+router.post('/new', feedbackController.submitFeedback);
+router.get('/:jobId', feedbackController.getFeedbackForJob);
+
+module.exports = router;
