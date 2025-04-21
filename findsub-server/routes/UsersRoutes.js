a4aeb3e1feb7587bafd4bf530da742a0f5145b66
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:40:52 +0100

    Update UsersRoutes.js

--- findsub-server/routes/UsersRoutes.js
+++ findsub-server/routes/UsersRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
 
+router.get('/:id', usersController.getUserById);
+router.post('/update/:id', usersController.updateUser);
+router.post('/profile-pic/:id', usersController.uploadProfilePic);
+router.get('/public/:id', usersController.getPublicProfile);
+
+module.exports = router;
