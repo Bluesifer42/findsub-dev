c4d4cd1f28a5300165ac45a5ab57bee5901ff165
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:48:11 +0100

    Create DevToolsController.js

---
+++ findsub-server/controllers/DevToolsController.js
@@ -0,0 +1,7 @@
+exports.createTestUser = (req, res) => {
+  res.send('createTestUser not implemented');
+};
+
+exports.createTestJob = (req, res) => {
+  res.send('createTestJob not implemented');
+};
