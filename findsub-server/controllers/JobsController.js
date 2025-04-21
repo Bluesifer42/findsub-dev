5c64f1c4ee6f94872ef7729aad68ce3468da8093
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:36:35 +0100

    Create JobsController.js

---
+++ findsub-server/controllers/JobsController.js
@@ -0,0 +1,40 @@
+exports.createJob = (req, res) => {
+  // handle job creation
+  res.send('createJob not implemented');
+};
+
+exports.getJobById = (req, res) => {
+  res.send('getJobById not implemented');
+};
+
+exports.editJob = (req, res) => {
+  res.send('editJob not implemented');
+};
+
+exports.updateJobStatus = (req, res) => {
+  res.send('updateJobStatus not implemented');
+};
+
+exports.getUserJobs = (req, res) => {
+  res.send('getUserJobs not implemented');
+};
+
+exports.getAppliedJobs = (req, res) => {
+  res.send('getAppliedJobs not implemented');
+};
+
+exports.getFilledJobs = (req, res) => {
+  res.send('getFilledJobs not implemented');
+};
+
+exports.applyToJob = (req, res) => {
+  res.send('applyToJob not implemented');
+};
+
+exports.selectSub = (req, res) => {
+  res.send('selectSub not implemented');
+};
+
+exports.retractApplication = (req, res) => {
+  res.send('retractApplication not implemented');
+};
