aa5554cfef4d4c8b73d52d721959cb443601ca23
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:34:45 +0100

    Update JobsRoutes.js

--- findsub-server/routes/JobsRoutes.js
+++ findsub-server/routes/JobsRoutes.js
@@ -1,1 +1,5 @@
+const express = require('express'); const router = express.Router(); const jobsController = require('../controllers/jobsController');
 
+router.post('/', jobsController.createJob); router.get('/:id', jobsController.getJobById); router.put('/:id', jobsController.editJob); router.post('/status/:id', jobsController.updateJobStatus); router.get('/user/:userId', jobsController.getUserJobs); router.get('/applied/:userId', jobsController.getAppliedJobs); router.get('/filled/:userId', jobsController.getFilledJobs); router.post('/apply/:jobId', jobsController.applyToJob); router.post('/select/:jobId', jobsController.selectSub); router.post('/retract/:jobId', jobsController.retractApplication);
+
+module.exports = router;
