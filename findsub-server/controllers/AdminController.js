d8ea2a96dc252bed2d99eab66fcaad555e9aed5a
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:45:44 +0100

    Create AdminController.js

---
+++ findsub-server/controllers/AdminController.js
@@ -0,0 +1,15 @@
+exports.getAllKinks = (req, res) => {
+  res.send('getAllKinks not implemented');
+};
+
+exports.createKink = (req, res) => {
+  res.send('createKink not implemented');
+};
+
+exports.updateKink = (req, res) => {
+  res.send('updateKink not implemented');
+};
+
+exports.deleteKink = (req, res) => {
+  res.send('deleteKink not implemented');
+};
