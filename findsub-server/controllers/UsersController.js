18aada13b1858150cfd2c778c489806a34f5e970
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:41:45 +0100

    Create UsersController.js

---
+++ findsub-server/controllers/UsersController.js
@@ -0,0 +1,15 @@
+exports.getUserById = (req, res) => {
+  res.send('getUserById not implemented');
+};
+
+exports.updateUser = (req, res) => {
+  res.send('updateUser not implemented');
+};
+
+exports.uploadProfilePic = (req, res) => {
+  res.send('uploadProfilePic not implemented');
+};
+
+exports.getPublicProfile = (req, res) => {
+  res.send('getPublicProfile not implemented');
+};
