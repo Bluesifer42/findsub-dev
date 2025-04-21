aa6707b0dfe1463604b184f7cf0ac32ee3a182e9
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 03:01:10 +0100

    Create index.js

---
+++ findsub-server/controllers/index.js
@@ -0,0 +1,15 @@
+const jobsController = require('./jobsController');
+const usersController = require('./usersController');
+const feedbackController = require('./feedbackController');
+const adminController = require('./adminController');
+const devToolsController = require('./devToolsController');
+const authController = require('./authController');
+
+module.exports = {
+  jobsController,
+  usersController,
+  feedbackController,
+  adminController,
+  devToolsController,
+  authController,
+};
