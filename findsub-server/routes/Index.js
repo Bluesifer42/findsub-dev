530798c133e22eb16fdb3f14a05d7913adf0c4eb
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 03:00:33 +0100

    Create Index.js

---
+++ findsub-server/routes/Index.js
@@ -0,0 +1,15 @@
+const JobsRoutes = require('./JobsRoutes');
+const UsersRoutes = require('./UsersRoutes');
+const FeedbackRoutes = require('./FeedbackRoutes');
+const AdminRoutes = require('./AdminRoutes');
+const DevToolsRoutes = require('./DevToolsRoutes');
+const AuthRoutes = require('./AuthRoutes');
+
+module.exports = {
+  JobsRoutes,
+  UsersRoutes,
+  FeedbackRoutes,
+  AdminRoutes,
+  DevToolsRoutes,
+  AuthRoutes,
+};
