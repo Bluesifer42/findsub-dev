5b31ab00d194c2c4f5c7dfb144e04759bce473ef
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 02:43:56 +0100

    Create FeedbackController.js

---
+++ findsub-server/controllers/FeedbackController.js
@@ -0,0 +1,7 @@
+exports.submitFeedback = (req, res) => {
+  res.send('submitFeedback not implemented');
+};
+
+exports.getFeedbackForJob = (req, res) => {
+  res.send('getFeedbackForJob not implemented');
+};
