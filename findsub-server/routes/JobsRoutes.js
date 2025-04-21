c80d09c8334c171c74e0e40dccfb70385a406c35
Bluesifer42 <bluesifer42@gmail.com>
2025-04-21 03:06:24 +0100

    Update JobsRoutes.js

--- findsub-server/routes/JobsRoutes.js
+++ findsub-server/routes/JobsRoutes.js
@@ -1,5 +1,16 @@
-const express = require('express'); const router = express.Router(); const jobsController = require('../controllers/jobsController');
+const express = require('express');
+const router = express.Router();
+const jobsController = require('../controllers/jobsController');
 
-router.post('/', jobsController.createJob); router.get('/:id', jobsController.getJobById); router.put('/:id', jobsController.editJob); router.post('/status/:id', jobsController.updateJobStatus); router.get('/user/:userId', jobsController.getUserJobs); router.get('/applied/:userId', jobsController.getAppliedJobs); router.get('/filled/:userId', jobsController.getFilledJobs); router.post('/apply/:jobId', jobsController.applyToJob); router.post('/select/:jobId', jobsController.selectSub); router.post('/retract/:jobId', jobsController.retractApplication);
+router.post('/', jobsController.createJob);
+router.get('/:id', jobsController.getJobById);
+router.put('/:id', jobsController.editJob);
+router.post('/status/:id', jobsController.updateJobStatus);
+router.get('/user/:userId', jobsController.getUserJobs);
+router.get('/applied/:userId', jobsController.getAppliedJobs);
+router.get('/filled/:userId', jobsController.getFilledJobs);
+router.post('/apply/:jobId', jobsController.applyToJob);
+router.post('/select/:jobId', jobsController.selectSub);
+router.post('/retract/:jobId', jobsController.retractApplication);
 
 module.exports = router;
